bc2c59f8290fd97ab71eb094f8ed8cde2df91bd1
Jimmy <me@jimmy.nz>
2022-02-23 10:39:01 +0000

    Print json

--- app/main.py
+++ app/main.py
@@ -1,6 +1,6 @@
 from fastapi import FastAPI, Request, Depends
 from fastapi_responses import custom_openapi
-from app.dependencies import auth_hook, auth_web, check_ref
+from app.dependencies import auth_hook, auth_web
 
 app = FastAPI()
 
@@ -9,7 +9,8 @@ app.openapi = custom_openapi(app)
 @app.get("/", dependencies=[Depends(auth_web)])
 @app.post("/", dependencies=[Depends(auth_hook)])
 async def hook(req: Request):
-    print("Update")
+    json = await req.json()
+    print(json)
     return "Update"
 
 
