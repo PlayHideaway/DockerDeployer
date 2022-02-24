c760f999a75405ac3e013a6ce66a214080e04d6e
Jimmy <me@jimmy.nz>
2022-02-24 03:47:16 +0000

    Pull and update container

--- app/main.py
+++ app/main.py
@@ -1,6 +1,9 @@
 from fastapi import FastAPI, Request, Depends
 from fastapi_responses import custom_openapi
 from app.dependencies import auth_hook, auth_web
+from os import getenv
+import subprocess
+from fastapi.exceptions import HTTPException
 
 app = FastAPI()
 
@@ -9,8 +12,17 @@ app.openapi = custom_openapi(app)
 @app.get("/", dependencies=[Depends(auth_web)])
 @app.post("/", dependencies=[Depends(auth_hook)])
 async def hook(req: Request):
-    json = await req.json()
-    print(json)
+    #json = await req.json()
+    #print(json)
+    service = getenv('SERVICE')
+    print(service)
+    resp = subprocess.run(["docker-compose", "-f", "docker-compose.yaml", "pull", service])
+    if resp.returncode != 0:
+        raise HTTPException(status_code=500, detail="Failed to pull Docker image")
+    print(resp.returncode)
+    resp = subprocess.run([f"docker-compose -f docker-compose.yaml up -d {service}"])
+    if resp.returncode != 0:
+        raise HTTPException(status_code=500, detail="Failed to relaunch container")
     return "Update"
 
 
